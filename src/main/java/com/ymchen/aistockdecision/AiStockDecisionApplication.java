2f7186790751dac2f86d84c6c0791899a096db84
chenym <mingzi411@163.com>
2021-01-31 16:16:24 +0800

    feat:project init

---
+++ src/main/java/com/ymchen/aistockdecision/AiStockDecisionApplication.java
@@ -0,0 +1,13 @@
+package com.ymchen.aistockdecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStockDecisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiStockDecisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ymchen/aistockdecision/AiStockDecisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymchen.aistockdecision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStockDecisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
